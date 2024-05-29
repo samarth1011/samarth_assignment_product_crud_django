4f16ec64b5d4f01f7de4519b81e30659020f0c8b
Samarth Godase <Samarth.godase@yardi.com>
2024-05-29 16:26:02 +0530

    follow plural noun for urls as 1 of best practice

--- inventory_management/api/urls.py
+++ inventory_management/api/urls.py
@@ -5,15 +5,15 @@ urlpatterns = [
     
     path('', views.ApiInfo.as_view(), name='list_all_api'),
     
-    path('product/', views.ProductListCreate.as_view(), name='product_view_create'),
+    path('products/', views.ProductListCreate.as_view(), name='product_view_create'),
     
-    path('product/<str:pk>/', views.ProductUpdateDelete.as_view(), name='product_update_delete'),
+    path('products/<str:pk>/', views.ProductUpdateDelete.as_view(), name='product_update_delete'),
 
-    path('product_by_id/<str:id>/', views.FindProductById.as_view(), name='product_find_by_id'),
+    path('products_by_id/<str:id>/', views.FindProductById.as_view(), name='product_find_by_id'),
 
-    path('product_by_title/<str:title>/', views.FindProductByTitle.as_view(), name='product_find_by_title'),
+    path('products_by_title/<str:title>/', views.FindProductByTitle.as_view(), name='product_find_by_title'),
     
-    path('category/<str:category>/', views.FindProductByCategory.as_view(), name='product_find_by_category'),
+    path('categorys/<str:category>/', views.FindProductByCategory.as_view(), name='product_find_by_category'),
 
-    path('product_price_greater_then/<str:price>/', views.FindProductPriceGreaterThan.as_view(), name='product_price_greater_then'),
+    path('products_price_greater_then/<str:price>/', views.FindProductPriceGreaterThan.as_view(), name='product_price_greater_then'),
 ]
